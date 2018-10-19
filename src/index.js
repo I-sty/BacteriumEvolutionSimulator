7b7c07367a49e181cab78de3d8e9dc6cb8ef825f
Szollosi <istvan.szollosi@accenture.com>
2018-10-19 10:51:42 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import Bacterium from './bacterium'
 import * as d3 from 'd3'
 
-var numberOfBactieriums = 10;
+var numberOfBactieriums = 40;
 var circleRadius = 5;
 var bacteriumsData = new Array(numberOfBactieriums);
 
 console.log("elso log")
 
 for (let i = 0; i < bacteriumsData.length; ++i) {
-    let x = Math.random() * 1000;
-    let y = Math.random() * 1000;
+    let x = Math.random() * 500;
+    let y = Math.random() * 500;
     let energy = Math.random() * 25;
     console.log(energy)
     bacteriumsData[i] = new Bacterium(x, y, energy);
