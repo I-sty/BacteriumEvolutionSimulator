6db0dc46d8c6554cbb5b7f2e9ceecf72eb4a21f5
Szollosi <istvan.szollosi@accenture.com>
2018-10-19 10:30:57 +0300

    first commit

---
+++ src/bacterium.js
@@ -0,0 +1,7 @@
+export default class Bacterium {
+  constructor(x, y, energy) {
+    this.x = x;
+    this.y = y;
+    this.energy = energy;
+  }
+} 
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import Bacterium from './bacterium'
+import * as d3 from 'd3'
+
+var numberOfBactieriums = 10;
+var circleRadius = 5;
+var bacteriumsData = new Array(numberOfBactieriums);
+
+console.log("elso log")
+
+for (let i = 0; i < bacteriumsData.length; ++i) {
+    let x = Math.random() * 1000;
+    let y = Math.random() * 1000;
+    let energy = Math.random() * 25;
+    console.log(energy)
+    bacteriumsData[i] = new Bacterium(x, y, energy);
+}
+
+var bacteriums = d3.select("svg")
+    .selectAll("g")
+    .data(bacteriumsData)
+    .enter()
+    .append("g")
+    .attr("class", "bacteriums")
+    .attr("transform", function (d) {
+        return "translate(" + d.x + "," + d.y + ")";
+    })
+
+bacteriums.append("circle").attr("r", circleRadius);
+
